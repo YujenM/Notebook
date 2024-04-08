5ee57b2790357b3474fcb05d01a980a287098c09
YujenM <maharjanyuzen@gmail.com>
2024-04-08 18:14:38 +0545

    update

---
+++ NotebookBackend/db.js
@@ -0,0 +1,14 @@
+// db.js
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB successfully");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
+};
+
+module.exports = connectToMongo;
